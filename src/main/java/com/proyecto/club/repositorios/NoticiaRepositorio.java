d57eef658e754561da40f8a71e5680c9fc451429
javiercaliz <javicaliz00@gmail.com>
2021-09-07 21:31:40 -0300

    completé servicios y repositorio de noticia

--- src/main/java/com/proyecto/club/repositorios/NoticiaRepositorio.java
+++ src/main/java/com/proyecto/club/repositorios/NoticiaRepositorio.java
@@ -5,6 +5,8 @@
  */
 package com.proyecto.club.repositorios;
 
+import java.util.List;
+
 import com.proyecto.club.entidades.Noticia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,8 +14,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
- * @author W7
+ * @author Javi
  */
 public interface NoticiaRepositorio extends JpaRepository<Noticia, String>{
     
+    List<Noticia>findByTituloContainingOrDescripcionContaining(String titulo, String descripcion);
+
 }
