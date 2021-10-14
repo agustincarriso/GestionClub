a856bfd4ce551dfafcaf89e9f892112a886ccf75
javiercaliz <javicaliz00@gmail.com>
2021-10-14 15:06:55 -0300

    controlador fotos

---
+++ src/main/java/com/proyecto/club/controladores/FotoController.java
@@ -0,0 +1,43 @@
+package com.proyecto.club.controladores;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.proyecto.club.entidades.Usuario;
+import com.proyecto.club.excepciones.WebException;
+import com.proyecto.club.servicios.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author S
+ */
+@Controller
+@RequestMapping("/foto")
+public class FotoController {
+    
+    @Autowired
+    public UsuarioService usuarioService;
+    
+    @GetMapping("/usuario")
+    public ResponseEntity<byte[]> fotoUsuario(@RequestParam String id) throws WebException{
+        try {
+            Usuario usuario = usuarioService.encontrarPorId(id);
+            byte[] foto = usuario.getFoto().getContenido();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG);
+            return new ResponseEntity<>(foto, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            Logger.getLogger(FotoController.class.getName()).log(Level.SEVERE, null, e);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
+}
